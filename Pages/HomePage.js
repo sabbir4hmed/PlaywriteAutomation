251b35a6fffa926aa19bd614168d7ff0f5103029
SABBIR <sadik3030@gmail.com>
2025-08-27 01:04:14 +0600

    Added some config

--- Pages/HomePage.js
+++ Pages/HomePage.js
@@ -7,35 +7,19 @@ class HomePage {
         this.practiceLink = page.locator("//a[normalize-space()='Practice']");
     }
 
-    async goto() {
-
-        await
-
-        allure.step('Navigating to Home Page', async () => {{
-             await
-            this.page.goto("https://practicetestautomation.com/");
-
-        await
-            this.page.waitForLoadState('networkidle');
-
+async goto() {
+    await allure.step('Navigating to Home Page', async () => {
+        await this.page.goto("https://practicetestautomation.com/");
+        await this.page.waitForLoadState('networkidle');
     });
-
 }
 
     async clickpracticelink() {
-        await
-            this.practiceLink.click();
-
-        await
-            this.page.waitForLoadState('networkidle');
-
-        await
-             this.page.waitForTimeout(2000);
-
+        await this.practiceLink.click();
+        await this.page.waitForLoadState('networkidle');
+        await this.page.waitForTimeout(2000);
     }
-}
-module.exports =
-{
-    HomePage
+
 }
 
+module.exports = { HomePage };
